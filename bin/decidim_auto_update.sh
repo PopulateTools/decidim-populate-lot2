9ec167492633c31f398015560a840b3929ae6953
Hugoren Martinako <aumpfbahn@gmail.com>
2024-06-03 14:16:51 +0200

    Force decidim git repo update

--- bin/decidim_auto_update.sh
+++ bin/decidim_auto_update.sh
@@ -7,7 +7,7 @@ function log() {
   echo -e "\n==== $1 ===="
 }
 
-log "Setup bundler and dependencies" && bundle config --delete frozen && bundle install && bundle lock --add-platform x86_64-linux
+log "Setup bundler and dependencies" && bundle config --delete frozen && bundle update --source decidim && bundle install && bundle lock --add-platform x86_64-linux
 log "Installing decidim new migrations" && bin/rails decidim:choose_target_plugins && bin/rails railties:install:migrations
 
 exit 0
