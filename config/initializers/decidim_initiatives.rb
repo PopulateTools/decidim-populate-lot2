02a5e0d0591bcb22679d2f84ca1bbba8f26877fe
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2025-01-15 21:36:18 +0100

    Add workflows and update their partials

--- config/initializers/decidim_initiatives.rb
+++ config/initializers/decidim_initiatives.rb
@@ -4,8 +4,21 @@ Decidim::Initiatives::Signatures.register_workflow(:dummy_signature_handler) do 
   workflow.form = "DummySignatureHandler"
   workflow.authorization_handler_form = "DummyAuthorizationHandler"
   workflow.action_authorizer = "DummySignatureHandler::DummySignatureActionAuthorizer"
-  # workflow.promote_authorization_validation_errors = true
   workflow.sms_verification = true
   workflow.sms_mobile_phone_validator = "DummySmsMobilePhoneValidator"
   workflow.sms_mobile_phone_form = "DummySmsMobilePhoneForm"
+end
+
+Decidim::Initiatives::Signatures.register_workflow(:dummy_signature_with_sms_handler) do |workflow|
+  workflow.form = "Decidim::Initiatives::SignatureHandler"
+  workflow.sms_verification = true
+  workflow.sms_mobile_phone_form = "DummySmsMobilePhoneForm"
+end
+
+Decidim::Initiatives::Signatures.register_workflow(:dummy_signature_with_personal_data_handler) do |workflow|
+  workflow.form = "DummySignatureHandler"
+  workflow.authorization_handler_form = "DummyAuthorizationHandler"
+  workflow.action_authorizer = "DummySignatureHandler::DummySignatureActionAuthorizer"
+  workflow.promote_authorization_validation_errors = true
+  workflow.save_authorizations = false
 end
