e5352796d2262c8b023bd90758260ef2ec82c627
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2025-01-20 11:44:20 +0100

    Split workflows

--- config/initializers/decidim_initiatives.rb
+++ config/initializers/decidim_initiatives.rb
@@ -19,6 +19,12 @@ Decidim::Initiatives::Signatures.register_workflow(:dummy_signature_with_persona
   workflow.form = "DummySignatureHandler"
   workflow.authorization_handler_form = "DummyAuthorizationHandler"
   workflow.action_authorizer = "DummySignatureHandler::DummySignatureActionAuthorizer"
+end
+
+Decidim::Initiatives::Signatures.register_workflow(:dummy_signature_with_personal_data_and_previous_authorization_handler) do |workflow|
+  workflow.form = "DummySignatureHandler"
+  workflow.authorization_handler_form = "DummyAuthorizationHandler"
+  workflow.action_authorizer = "DummySignatureHandler::DummySignatureActionAuthorizer"
   workflow.promote_authorization_validation_errors = true
   workflow.save_authorizations = false
 end
